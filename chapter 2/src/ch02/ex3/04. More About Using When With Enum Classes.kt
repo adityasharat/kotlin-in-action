d3a1faae022432487cab6c4c61a46f08b191c23b
Aditya Sharat <adityasharat@fb.com>
2018-10-13 10:02:40 +0100

    add 2.3.4

---
+++ chapter 2/src/ch02/ex3/04. More About Using When With Enum Classes.kt
@@ -0,0 +1,11 @@
+package ch02.ex3
+
+fun getWarmth(color: Color) = when(color) {
+    Color.RED, Color.ORANGE, Color.YELLOW -> "warm"
+    Color.GREEN -> "neutral"
+    Color.BLUE, Color.INDIGO, Color.VIOLET -> "cold"
+}
+
+fun main(args: Array<String>) {
+    Color.values().forEach { c -> println( "$c : " + getWarmth(c)) }
+}
