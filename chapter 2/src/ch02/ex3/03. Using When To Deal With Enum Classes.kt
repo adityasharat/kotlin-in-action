d69357c480a68cdc8465acb039cf534b96048913
Aditya Sharat <adityasharat@fb.com>
2018-10-13 09:57:38 +0100

    add 2.3.3

---
+++ chapter 2/src/ch02/ex3/03. Using When To Deal With Enum Classes.kt
@@ -0,0 +1,16 @@
+package ch02.ex3
+
+fun getMnemonic(color: Color) =
+        when (color) {
+            Color.RED -> "Richard"
+            Color.ORANGE -> "Of"
+            Color.YELLOW -> "York"
+            Color.GREEN -> "Gave"
+            Color.BLUE -> "Battle"
+            Color.INDIGO -> "In"
+            Color.VIOLET -> "Vain"
+        }
+
+fun main(args: Array<String>) {
+    Color.values().forEach { c -> print(getMnemonic(c) + " ") }
+}
